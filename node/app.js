5b88b7cb050b5d4c0a44c63d4d30f9fe9dc713e9
JosephNaa <js.pekah@gmail.com>
2021-03-19 10:40:42 +0900

    connect socket server

--- node/app.js
+++ node/app.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3030
 
 app.get('/', (_, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log('Express listening on port', port)
+})
+
+const listen = require('socket.io')
+const io = listen(server)
+
+io.on('connection', (socket) => {
+    console.log('socket server connected')
+    
+    socket.on('client message', (data) => {
+        console.log(data)
+
+        io.emit('server message', {
+            mesaage: data.message
+        })
+    })
 })
